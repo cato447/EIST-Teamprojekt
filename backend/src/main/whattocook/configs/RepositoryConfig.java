f6385b40f644c696ea82d806b2367383c491eb8d
cato <47259183+cato447@users.noreply.github.com>
2022-06-02 19:11:46 +0200

    Proof of concept (#6)
    * Sample template created
    
    * added findByName functionality for item
    
    * Solve Cors errors and inhibit DefaultExposure
    
    * changed project structure
    
    * Added frontend
    
    * Creation of base template (#1)
    
    * changed base path of REST api and updated frontend api quering

---
+++ backend/src/main/whattocook/configs/RepositoryConfig.java
@@ -0,0 +1,19 @@
+package whattocook.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import whattocook.models.Item;
+
+@Configuration
+class RepositoryConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Item.class);
+        config.setBasePath("/api/v1");
+    }
+
+}
+
---
+++ backend/src/main/whattocook/repositories/ItemRepository.java
@@ -0,0 +1,12 @@
+package whattocook.repositories;
+
+import whattocook.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Optional<Item> findByName(String name);
+}
